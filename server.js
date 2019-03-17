668a87992aa1f3f13d576a8bee3693b89495766e
Noor <noornabiji4@gmail.com>
2019-03-17 23:17:30 +0530

    server js file update

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+//body parser middleware 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -15,8 +21,13 @@ mongoose.connect(db)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello RockstaR'));
+//password middleware
+app.use(passport.initialize())
 
+//password Config
+require('./config/passport')(passport)
+
+// USe routes
 app.use('/api/users', users);
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
