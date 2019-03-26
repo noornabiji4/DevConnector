71446319eef82708fac09d1514437b1f378b1b21
Noor <noornabiji4@gmail.com>
2019-03-26 22:59:23 +0530

    validation register

--- validation/register.js
+++ validation/register.js
@@ -12,8 +12,6 @@ module.exports = function validateRegisterInput(data) {
     if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
         errors.name = 'Name must be between 2 and 30 characters';
     }
-
-   
     if (Validator.isEmpty(data.email)) {
         errors.email = 'Email field is required';
     }
