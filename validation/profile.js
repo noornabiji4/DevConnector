f078ef9d7241a70a75d733f52b68dce3f74c0a2d
Noor <noornabiji4@gmail.com>
2019-06-13 23:58:16 +0530

    update

--- validation/profile.js
+++ validation/profile.js
@@ -26,37 +26,37 @@ module.exports = function validateProfileInput(data) {
 
     if (!isEmpty(data.website)) {
         if (!Validator.isURL(data.website)) {
-            errors.website = 'Not a valid URl';
+            errors.website = 'Not a valid URL';
         }
     }
 
     if (!isEmpty(data.youtube)) {
         if (!Validator.isURL(data.youtube)) {
-            errors.youtube = 'Not a valid URl';
+            errors.youtube = 'Not a valid URL';
         }
     }
 
     if (!isEmpty(data.twitter)) {
         if (!Validator.isURL(data.twitter)) {
-            errors.twitter = 'Not a valid URl';
+            errors.twitter = 'Not a valid URL';
         }
     }
 
     if (!isEmpty(data.facebook)) {
         if (!Validator.isURL(data.facebook)) {
-            errors.facebook = 'Not a valid URl';
+            errors.facebook = 'Not a valid URL';
         }
     }
 
     if (!isEmpty(data.linkedin)) {
         if (!Validator.isURL(data.linkedin)) {
-            errors.linkedin = 'Not a valid URl';
+            errors.linkedin = 'Not a valid URL';
         }
     }
 
     if (!isEmpty(data.instagram)) {
         if (!Validator.isURL(data.instagram)) {
-            errors.instagram = 'Not a valid URl';
+            errors.instagram = 'Not a valid URL';
         }
     }
 
