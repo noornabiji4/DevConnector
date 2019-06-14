f5cd233427604ebf92a213a5fc2c7898c3d5ea33
Noor <noornabiji4@gmail.com>
2019-06-14 23:38:50 +0530

    update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -23,6 +23,7 @@ router.get('/test', (req, res) => res.json({ msg: "Profile works" }));
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     const errors = {}
     Profile.findOne({ user: req.user.id })
+    .populate('user' , ['name', 'avatar '])
         .then(profile => {
             if (!profile) {
                 errors.noprofile = 'There is no profile for this user ';
@@ -50,6 +51,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
     if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
     if (req.body.location) profileFields.location = req.body.location;
     if (req.body.bio) profileFields.bio = req.body.bio;
     if (req.body.status) profileFields.status = req.body.status;
@@ -67,16 +69,19 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
-    Profile.findOne({ user: req.body.id }).then(profile => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
         if (profile) {
             //update
-            Profile.findOneAndDelete(
+            Profile.findOneAndUpdate(
                 { user: req.user.id },
                 { $set: profileFields },
                 { new: true }
             ).then(profile => res.json(profile))
         }
         else {
+            //create 
+
+            // check if handle exist
             Profile.findOne({ handle: profileFields.handle }).then(profile => {
                 if (profile) {
                     errors.handle = 'That handle already exists';
