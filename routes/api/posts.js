2ba0e253f8cb2601ec10a431360ee6777b049320
Noor <noornabiji4@gmail.com>
2019-06-16 23:56:17 +0530

    posts route data

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passort = require('passport');
+
+const Post = require('../../models/Posts')
 
 //@routes GET api/posts/test
 //desc Tests post route
 //@access public
 
 router.get('/test', (req, res) => res.json({ msg: "Posts works" }));
+
+//@routes   POST api/posts
+//desc      Tests post route
+//@access   Private
+router.post('/', passort.authenticate('jwt', { session: false }), (req, res) => {
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.body.user
+    })
+    newPost.save().then(post => res.json(post));
+
+})
 
 module.exports = router;
