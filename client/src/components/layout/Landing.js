1af5702797dd99a2954c2f9eb310ffad8eb3f7cc
Noor <noornabiji4@gmail.com>
2019-06-25 23:45:07 +0530

    update layout

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
     render() {
         return (
-            <div class="landing">
-                <div class="dark-overlay landing-inner text-light">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12 text-center">
-                                <h1 class="display-3 mb-4">Developer Connector </h1>
-                                <p class="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+            <div className="landing">
+                <div className="dark-overlay landing-inner text-light">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12 text-center">
+                                <h1 className="display-3 mb-4">Developer Connector </h1>
+                                <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
                                 <hr />
-                                <a href="register.html" class="btn btn-lg btn-info mr-2">Sign Up</a>
-                                <a href="login.html" class="btn btn-lg btn-light">Login</a>
+                                <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
+                                <Link to="/login" className="btn btn-lg btn-light">Login</Link>
                             </div>
                         </div>
                     </div>
