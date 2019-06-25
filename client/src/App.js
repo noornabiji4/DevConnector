3df42b62b2f84a6a3fc96a54101f8798389e23dc
Noor <noornabiji4@gmail.com>
2019-06-25 23:44:31 +0530

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store'
 
 import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer' ;
+import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
+
 class App extends Component {
-  render(){
+  render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Provider store={store} >
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     )
   }
 }
