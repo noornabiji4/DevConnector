766e164364dfcc159da373aae200292767761e26
Noor <noornabiji4@gmail.com>
2019-06-27 22:44:31 +0530

    auth authentication

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
+
 import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 //Register user  
@@ -47,4 +48,14 @@ export const setCurrentUser = (decoded) => {
         type: SET_CURRENT_USER,
         payload: decoded
     }
+}
+
+// Log user out
+export const logoutUser = () => dispatch => {
+    // Remove token from localStorage
+    localStorage.removeItem('jwtToken');
+    // Remove auth header to feature request
+    setAuthToken(false);
+    // Set current user to {} which will set isAuthenticated to false 
+    dispatch(setCurrentUser({}))
 }
