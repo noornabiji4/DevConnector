9579014841eedf5946b3b6ee48c7830a4ec3c8c9
Noor <noornabiji4@gmail.com>
2019-06-27 22:44:02 +0530

    update main file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store'
 
@@ -10,6 +14,28 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
 import './App.css';
+
+// Chcek token 
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expire token 
+  const currentTime = Date.now() / 1000
+  if(decoded.exp < currentTime){
+    // Logout User
+    store.dispatch(logoutUser())
+      //TODO: Clear current profile 
+
+      // Redirect to login    
+      window.location.href = '/login'
+
+  }
+}
 
 
 class App extends Component {
