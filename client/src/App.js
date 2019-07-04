4b93a0f6bef430fca841c854f90bb442804344fb
Noor <noornabiji4@gmail.com>
2019-07-04 22:10:47 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -9,12 +9,16 @@ import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from 'react-redux';
 import store from './store'
 
+import PrivateRoute from './components/common/PrivateRoute';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+
 
 
 import './App.css';
@@ -30,14 +34,14 @@ if (localStorage.jwtToken) {
 
   // Check for expire token 
   const currentTime = Date.now() / 1000
-  if(decoded.exp < currentTime){
+  if (decoded.exp < currentTime) {
     // Logout User
     store.dispatch(logoutUser())
-      //TODO: Clear current profile 
-      store.dispatch(clearCurrentProfile)
+    //TODO: Clear current profile 
+    store.dispatch(clearCurrentProfile)
 
-      // Redirect to login    
-      window.location.href = '/login'
+    // Redirect to login    
+    window.location.href = '/login'
 
   }
 }
@@ -54,7 +58,12 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+              </Switch>
             </div>
             <Footer />
           </div>
